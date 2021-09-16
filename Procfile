41ea793d2584e6aac3f4762953108161aa94b851
jinjun zhen <jinjunzhen@yahoo.com>
2021-09-16 14:43:05 -0500

    server.js updated comments

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node server.js
+// this file tells heroku to run specific file
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
