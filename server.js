b8e681f431a07f66abeb88c2ff689472245571d7
jinjun zhen <jinjunzhen@yahoo.com>
2021-09-16 14:39:36 -0500

    server.js updated

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ const local_url = "http://localhost:4200/";
 const jjz_heroku = "https://jjz-animal-crossing.herokuapp.com/";
 const toddy_heroku = "";
 
+//setting cors url
 app.use(cors({
   origin : local_url, jjz_heroku, toddy_heroku
 }));
 
+
+
+//connect mongoDB 
 mongo_url = "mongodb+srv://user:p0tat0e@animalcrossingline.5id29.mongodb.net/ac";
 
+mongoose.connect(mongo_url).catch(function (reason) {
+  console.log('Unable to connect to the mongodb instance. Error: ', reason);
+});
 
 
+//---------------------------------------------------------------- Fish, Bug, Ocean, schema
 const fishSchema = new mongoose.Schema({
 	id: Number,
 	name: String,
@@ -26,14 +34,20 @@ const fishSchema = new mongoose.Schema({
 	timeArray: [],
 	months: [],
 });
-
 const Fish = mongoose.model("Fish", fishSchema);
 
-mongoose.connect(mongo_url).catch(function (reason) {
-  console.log('Unable to connect to the mongodb instance. Error: ', reason);
-});
 
 
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------  Fish, Bug, Ocean --- CRUD
 app.get("/api", (req, res) => {
   Fish.find((err, data) => {
     if (err) {
@@ -46,7 +60,7 @@ app.get("/api", (req, res) => {
 });
 
 
-
+//----------------------------------------------------------------  Launched Angular index.html 
 app.use(express.static(__dirname + '/dist/animal-crossing'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname +
@@ -54,6 +68,8 @@ app.get('/', function (req, res) {
 });
 
 
+
+//----------------------------------------------------------------  Holding backend server
 app.listen(process.env.PORT || 3000, ()=>{
   console.log("service is on" );
 });
