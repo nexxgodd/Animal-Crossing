4734c7e86894b76e25fd9c9de13b968a4301c601
Todd <nexxgodd@gmail.com>
2021-09-17 07:54:42 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/app/ac.service.ts
+++ src/app/ac.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {Fish,Bug,SeaCreature} from 'src/models/Creatures';
 
-//const base_url = "http://localhost:3000/";
-//const heroku_url = "http://localhost:8080/api/v1/ac/";
-//const heroku_url = "https://jjz-animal-crossing.herokuapp.com/";
-const heroku_url = "https://simple-ttt-api.herokuapp.com/api/v1/ac/";
+const URL = "http://localhost:3000/api/";
+//const URL = "http://localhost:8080/api/v1/ac/";
+//const URL = "https://jjz-animal-crossing.herokuapp.com/api/";
+// const URL = "https://simple-ttt-api.herokuapp.com/api/v1/ac/";
 
 
 @Injectable({
@@ -16,9 +16,12 @@ export class AcService {
 	constructor(private httpClient: HttpClient) { }
 
 	findFish(): Observable<Fish[]> {
-		return this.httpClient.get<Fish[]>(heroku_url + "fish");
+		return this.httpClient.get<Fish[]>(URL + "fish");
 	}
-	// findBugs(): Observable<Fish[]> {
-	// 	return this.httpClient.get<Fish[]>(heroku_url + "fish");
-	// }
+	findBug(): Observable<Bug[]> {
+		return this.httpClient.get<Bug[]>(URL + "bug");
+	}
+	findSea(): Observable<SeaCreature[]> {
+		return this.httpClient.get<SeaCreature[]>(URL + "sea");
+	}
 }
